19b22ca10619fd7e63df0c76f3835f1c94932ec5
Dan M <djmthedev@gmail.com>
2020-01-23 09:41:04 -0800

    string primitives

--- Pointers/Pointers/main.m
+++ Pointers/Pointers/main.m
@@ -10,8 +10,35 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        char letter = 't';
+        char nullChar = '\0'; // NULL "Mac\0"
+        printf("Letter: %c\n", letter);
+        printf("Null: %c\n", nullChar);
+        
+        
+        int count = 2;
+        printf("Count: %i\n", count);
+        
+        // floats
+        float float_pi = 3.1459265359;
+        double pi = 3.1459265359;
+        
+        printf("Float_pi %f\n", float_pi);
+        printf("pi %f\n", pi);
+        
+        
+        
+        
+        
     }
     return 0;
 }
+
+/*
+ char *ptr = "Hello World!";
+ NSLog(@"Pointer: %li", (long)ptr);
+ 
+ ptr += 3;
+ NSLog(@"Pointer: %li", (long)ptr);
+ */
