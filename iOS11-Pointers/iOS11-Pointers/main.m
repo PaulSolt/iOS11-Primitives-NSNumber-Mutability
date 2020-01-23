8bf2f9ce7e3e33601d28b299cc50eafbc3fac03d
Paul Solt <PaulSolt@gmail.com>
2020-01-23 12:16:54 -0500

    Added starter mac project

---
+++ iOS11-Pointers/iOS11-Pointers/main.m
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  iOS11-Pointers
+//
+//  Created by Paul Solt on 1/23/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
+
+
+//git init
+//
+//touch .gitignore
+//ls -al
+//open .gitignore
